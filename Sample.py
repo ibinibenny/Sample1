a219475600056423b7433b06eed9a8b0fa321d11
ibinibenny <48010643+ibinibenny@users.noreply.github.com>
2021-02-19 10:40:12 +0530

    updated

--- Sample.py
+++ Sample.py
@@ -1,1 +1,10 @@
-welcome to our world
+#Write a program to take a string as input and return a string with vowels removed.(implement List Comprehesion)
+str1,st=input("enter string: "),' '
+vow=['a','e','i','o','u','A','E','I','O','U']
+def vowremv(str2):
+	st=' '
+	vowremvd=[i for i in str2 if i not in vow]
+	for i in vowremvd:
+		st+=i
+	return st
+print(list(vowremv(str1)))
