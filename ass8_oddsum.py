2a090a37a6eceeb8ffe6df886729e71b48b4403a
ibinibenny <48010643+ibinibenny@users.noreply.github.com>
2021-02-20 14:42:56 +0530

    Update ass8_oddsum.py

--- ass8_oddsum.py
+++ ass8_oddsum.py
@@ -5,3 +5,14 @@ for i in range(1,50,2):
 	print(i)
 	sum1+=i
 print("the sum of above numbers=%d" %sum1)
+
+
+a=1
+b=50
+s=0
+print("odd numbers between",a, 'and',b,"are", end=" ")
+for num in range(a,b+1):
+	if num%2!=0:
+		print(num,end=" ")
+		s=s+num
+print("\nsum",s)
