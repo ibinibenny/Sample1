3df8dd3159c3d08e72325eb4f2e6fb8d49b2e5b3
ibinibenny <48010643+ibinibenny@users.noreply.github.com>
2021-02-26 14:53:57 +0530

    assignments-2_15

---
+++ ass2_strinsert.py
@@ -0,0 +1,6 @@
+#Write a Python program to insert a given string at the beginning of all items in a list. 
+l1=[1,2,3,'hlo','pleasure meeting u',68]
+print(l1,"\n-------------------------------------------------------------")
+for i in range(0,12,2):
+	l1.insert(i,'jeswin')
+print(l1)	
