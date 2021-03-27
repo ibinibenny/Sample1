6e19b3d0526a6f0fff480738822ff548080f5c70
ibinibenny <48010643+ibinibenny@users.noreply.github.com>
2021-03-27 14:08:38 +0530

    Add files via upload

---
+++ patterns/ass_patt3.py
@@ -0,0 +1,10 @@
+for i in range(65,70):
+	j=65
+	while j<i+1:	
+		print(chr(j),end=' ')
+		j+=1
+	print("\n")
+	
+	
+	
+		
---
+++ patterns/ass_patt5.py
@@ -0,0 +1,8 @@
+k=1
+for i in range(1,7):
+	for j in range(1,i+1):
+		print(k*2,end=' ')
+		k+=1
+	print("\n")
+	
+	
